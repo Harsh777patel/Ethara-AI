9ed597f09c2790f46c8248d03b33134365575603
Harsh777patel <patelpankaj7677@gmail.com>
2026-05-02 05:03:29 +0530

    .......

--- backend/index.js
+++ backend/index.js
@@ -52,12 +52,12 @@ app.use((err, _req, res, _next) => {
   });
 });
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`\n🚀 Server running on http://localhost:${PORT}`);
 //   console.log(`📦 Environment: ${process.env.NODE_ENV}`);
 // });
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
